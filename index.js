ed49f980ac33496dd29eb7b1567de316e09f3bec
Carter Ward <20886719+CarterJWard@users.noreply.github.com>
2022-08-25 16:42:12 +1000

    Frontend static build

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
+
 morgan.token('body', req => {
     return JSON.stringify(req.body)
 })
@@ -10,6 +11,7 @@ morgan.token('body', req => {
 app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :body'))
+app.use(express.static('build'))
 
 let persons = [
     {
